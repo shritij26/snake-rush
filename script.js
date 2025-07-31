cc5351ddf720dd12e8192364fa52faac64d72d68
shritij <shritijraman@gmail.com>
2025-07-31 13:30:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function startGame() {
   updateScore();
   placeFood();
   clearInterval(interval);
-  interval = setInterval(update, 20);
+  interval = setInterval(update, 200);
   gameOverDisplay.classList.add("hidden");
   render();
 }
